85e3da9c8262f33b2db95151b0cf6f463e29fb09
Дмитрий <torres-1792@mail.ru>
2025-02-07 14:08:27 +0300

    second lesson

---
+++ app/models/task.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.ddl import CreateTable
+from app.models.base import Base
+from app.models.user import User
+
+class Task(Base):
+    __tablename__ = "tasks"
+    id = Column(Integer, primary_key=True, index=True)
+    tittle = Column(String)
+    content = Column(String)
+    priority = Column(Integer, default=0)
+    completed = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False, index=True)
+    slug = Column(String, unique=True, index=True)
+    user = relationship("User", back_populates='tasks')
+
+
+print(CreateTable(Task.__table__))
